5759a8eec4227489641edd0329b916f9cee01f6e
Salil-IND <salil_programming@outlook.com>
2026-01-20 19:13:05 +0530

    Commit before fixing aside panel

---
+++ script.js
@@ -0,0 +1,12 @@
+//Structure of the data to be stored in local Storage
+//             {"2025":{
+//     "total":5000,
+//     "months":{
+//         "1":{
+//             "total":5000,
+//             "records":[
+//                 {"id":1245, "date":"20-01-2025", "amount":5000, "reason":"Bought a monitor"},
+//             ]
+//         }
+//     }
+// }}
